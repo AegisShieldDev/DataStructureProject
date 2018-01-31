e8f79d238306bed9dcdb69091f558b6835947e86
Outrider99 <outrider.w@gmail.com>
2018-01-31 11:40:29 -0700

    Initial commit - created runner and controller

---
+++ DataStructureProject/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  DataStructureProject
+//
+//  Created by Miller, Wyatt on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller/Controller.hpp"
+int main()
+{
+    Controller app;
+    app.start();
+    return 0;
+}
