dd8f885df00b21b96d7cc8da0c1dff25b6b07e0c
Outrider99 <outrider.w@gmail.com>
2018-01-31 12:12:09 -0700

    Built out Timer and the Controller

--- DataStructureProject/Controller/Controller.cpp
+++ DataStructureProject/Controller/Controller.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "Controller.hpp"
+
+using namespace std;
+
+void Controller :: start()
+{
+    Timer codeTimer;
+    codeTimer.startTimer();
+    cout << "Cooooooooode" << endl;
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
+    codeTimer.resetTimer();
+    codeTimer.startTimer();
+    
+    for(int index = 0; index < 500; index++)
+    {
+        cout << "Index is " << index << "\t";
+    }
+    
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
+}
