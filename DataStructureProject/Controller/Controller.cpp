95f35c9146bdd070598e80520735e68e861ce42c
Outrider99 <outrider.w@gmail.com>
2018-02-02 11:48:15 -0700

    crted findMaxAndMin

--- DataStructureProject/Controller/Controller.cpp
+++ DataStructureProject/Controller/Controller.cpp
@@ -12,21 +12,32 @@ using namespace std;
 
 void Controller :: start()
 {
-    Timer codeTimer;
-    codeTimer.startTimer();
-    cout << "Cooooooooode" << endl;
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
-    codeTimer.resetTimer();
-    codeTimer.startTimer();
+    findMaxAndMin()
+}
 
+void Controller :: findMaxAndMin()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
     vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/wmil5119/Desktop/C++/DataStructureProject/DataStructureProject/Data/crime.csv");
     
-    for(int i = 200; i <= 215; i++)
-    {
-        cout << i << " is " << myData[i] << endl;
-    }
+    int minIndex = 0;
+    int maxIndex = 0;
     
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
+    for(int index = 1; index < myData.size(); index++)
+    {
+        if(myData [minIndex] > myData [index])
+        {
+            minIndex = index;
+        }
+        
+        if(myData [maxIndex] < myData[index])
+        {
+            maxIndex = index;
+        }
+    }
+    searchTimer.stopTimer();
+    cout << "The smallest crime stat is at " << minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << "The largest crime stat is at "  << minIndex << " and it is: " << myData[minIndex] << endl;
+    searchTimer.displayInformation();
 }
