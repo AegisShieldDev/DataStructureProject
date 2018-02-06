27a684fba850e7a764a0d58d4eb2deac855981b1
Outrider99 <outrider.w@gmail.com>
2018-02-06 13:42:48 -0700

    Built out LinearNode

--- DataStructureProject/Model/Node/Node.hpp
+++ DataStructureProject/Model/Node/Node.hpp
@@ -25,24 +25,24 @@ template <class Type>
 Node<Type> :: Node()
 {
     
-}
+};
 
 template <class Type>
 Node<Type> :: Node(Type data)
 {
     this->data = data;
-}
+};
 
 template <classType>
 void Node<Type> :: setData(Type data)
 {
     this->data = data;
-}
+};
 
 template <class Type>
 void Node<Type> :: getData()
 {
     return data;
-}
+};
 
 #endif /* Node_h */
