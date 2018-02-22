b207ae69eade2c941b4b2ead02f8c27576d0e3df
Outrider99 <outrider.w@gmail.com>
2018-02-22 12:23:58 -0700

    created Stack, added linear file

---
+++ DataStructureProject/Model/Structures/Linear/Queue.h
@@ -0,0 +1,13 @@
+//
+//  Queue.h
+//  DataStructureProject
+//
+//  Created by Miller, Wyatt on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+
+#endif /* Queue_h */
