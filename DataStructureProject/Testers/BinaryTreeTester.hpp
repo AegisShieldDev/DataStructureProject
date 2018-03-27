34d0b16f12fa0d5a5719e11b3efb11495b03a33e
Outrider99 <outrider.w@gmail.com>
2018-03-27 12:59:53 -0600

    added binaryTreeTester

---
+++ DataStructureProject/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,21 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructureProject
+//
+//  Created by Miller, Wyatt on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Node/BinaryTreeNode.hpp"
+#include <ioStream>
+
+class BinaryTreeTester
+{
+private:
+public:
+}
+
+#endif /* BinaryTreeTester_hpp */
